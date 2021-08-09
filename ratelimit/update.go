1df390f1b19a5de92b3b2744df358c6890e56a2c
Jack Gledhill <me@jackgledhill.com>
2021-08-09 20:43:26 +0100

    Fix bug when ratelimit headers missing

--- ratelimit/update.go
+++ ratelimit/update.go
@@ -5,20 +5,21 @@ import (
 	"strconv"
 )
 
+// UpdateBucket will grab the ratelimit information from the response headers and use that to update the existing ratelimit bucket.
 func UpdateBucket(id string, res *http.Response) error {
 	remaining, err := strconv.Atoi(res.Header.Get("X-RateLimit-Remaining"))
 	if err != nil {
-		return err
+		return nil
 	}
 
 	reset, err := strconv.ParseFloat(res.Header.Get("X-RateLimit-Reset"), 64)
 	if err != nil {
-		return err
+		return nil
 	}
 
 	limit, err := strconv.Atoi(res.Header.Get("X-RateLimit-Limit"))
 	if err != nil {
-		return err
+		return nil
 	}
 
 	Buckets[id] = Bucket{

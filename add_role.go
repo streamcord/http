8ecd9dcb840720572d7f47fce35a88778d7b98ab
Jack Gledhill <me@jackgledhill.com>
2021-08-16 13:40:37 +0100

    Remove status code handling

--- add_role.go
+++ add_role.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 
@@ -18,17 +17,6 @@ func (c *Client) AddRole(gID string, uID string, rID string) (*http.Response, er
 	res, err := c.MakeRequest(req)
 	if err != nil {
 		return nil, err
-	}
-
-	if res.StatusCode >= 400 {
-		switch res.StatusCode {
-		case 400:
-			return nil, errors.New("malformed request payload")
-		case 403:
-			return nil, errors.New("missing required permission")
-		default:
-			return nil, fmt.Errorf("unknown error: %d", res.StatusCode)
-		}
 	}
 
 	return res, nil
--- send_message.go
+++ send_message.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 
@@ -20,17 +19,6 @@ func (c *Client) SendMessage(cID string, payload payloads.SendMessage) (*http.Re
 	res, err := c.MakeRequest(req)
 	if err != nil {
 		return nil, err
-	}
-
-	if res.StatusCode >= 400 {
-		switch res.StatusCode {
-		case 400:
-			return nil, errors.New("malformed request payload")
-		case 403:
-			return nil, errors.New("missing required permission")
-		default:
-			return nil, fmt.Errorf("unknown error: %d", res.StatusCode)
-		}
 	}
 
 	return res, nil
